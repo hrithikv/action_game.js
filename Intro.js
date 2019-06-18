c2c41ca85faf017830525d4541a393b536718517
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-17 23:03:20 -0400

    Create Intro.js

---
+++ Intro.js
@@ -0,0 +1,23 @@
+public class Intro {    
+    public String[] intro_list = new String[3];
+    float morality;
+    
+    public ChapterIntro( String Intro_pos, String Intro_neu, String Intro_neg, int currentMorality, int hist) {
+        intro_list[0] = Intro_pos;
+        intro_list[1] = Intro_neu;
+        intro_list[2] = Intro_neg;
+        val = (float) hist
+        morality = int (currentMorality / val) ;
+    }
+    
+    public String getIntro() {
+        int val_1 = int (1/3.0);
+        int val_2 = int (-1/3.0);
+        if( morality > val_1 ) 
+            return intro_list[0];
+        else if( morality < val_2 )
+            return intro_list[2];
+        else
+            return intro_list[1];
+    }
+}
