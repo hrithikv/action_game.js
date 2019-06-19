ec2f97a530ba12022c8da7375b9eacbfa6149712
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-19 18:47:02 -0400

    Update and rename structure.java to Structure.java

---
+++ game_structure/Structure.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Structure {
+    
+    protected ArrayList<Combatant> player_list;
+    protected Inventory settings;
+    
+    public Structure () {
+        player_list = new ArrayList();
+        settings = new Inventory();
+    }
+    
+    public ArrayList<Combatant> get_players () {
+        return player_list;
+    }
+    
+    public void add (Combatant player) {
+        player_list.add(player);
+    }
+    
+    public Combatant get_player (int id) {
+        return player_list.get(id);
+    }
+   
+   public boolean defeated () {
+        for (Combatant player : player_list) {
+            if (player.isAlive())
+                return false;
+        }
+        return true;
+   }
+     
+    public ArrayList<Combatant> players_alive () {
+        ArrayList<Combatant> players_alive = new ArrayList<Combatant>();
+        for (Combatant player : player_list) {
+            if (player.isAlive())
+                players_alive.add(player);
+        }
+        return players_alive;
+    }
+    
+}
