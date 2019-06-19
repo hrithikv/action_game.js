f336e2d5387bbe1ca8f3126cd773ab40463b7cb9
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-19 19:25:04 -0400

    Update Non-Playable.java

--- Playable/Non-Playable.java
+++ Playable/Non-Playable.java
@@ -3,11 +3,11 @@ public interface nonplayable {
     public void behavior ();
     
     public NonPlayable (String id) {
-        set_type(150);
+        set_type(100);
     }
     
     public NonPlayable (String id,String type,double reward) {
-        setHealth(150);
+        setHealth(100);
         set_type(type);
         set_reward(reward);
     }
