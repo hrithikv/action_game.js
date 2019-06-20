b6570b515c6ea949bb0e86e4500b8133135257a2
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-19 23:26:37 -0400

    Update Combat.java

--- Combat.java
+++ Combat.java
@@ -5,12 +5,12 @@ public class Combat {
     protected static String output = "";
     
     protected Team opponent_list;
-    protected ArrayList<Combatant> turnOrder;
+    protected ArrayList<Combatant> combatants_list;
     protected int turnCounter;
 
     public Combat () {
         opponent_list = new Team();
-        turnOrder = new ArrayList();
+        combatants_list = new ArrayList();
         val_init = 0;
         turnCounter = val_init;
     }
@@ -20,38 +20,36 @@ public class Combat {
     }
     
     public void battle () {
-        while (Game.getParty().defeated() || opponent_list.defeated()) {
-            turnOrderP();
-            if (turnOrder.get(0) instanceof NonPlayable)
+        game_id = Game.get_party();
+        while (game_id.defeated() || opponent_list.defeated()) {
+            combatants_list_P();
+            if (combatants_list.get(0) instanceof Non_Playable)
                 System.out.println("1");
                 
             else
-                System.out.print("What will " + turnOrder.get(0).getName() + " do?");
+                System.out.print("What is the next step?");
                 System.out.print();
-                System.out.print("A: Attack");
+                System.out.print("1: Attack");
                 System.out.print();
-                System.out.print("B: Skill");
-                System.out.print();
-                System.out.print("A: Item");
-                System.out.print();
-                System.out.print("A: Defend");
+                System.out.print("2: Skill");
                 System.out.print();
                 
-                output = Game.getScanner().nextLine().toUpperCase();
+                output = Game.getScanner().nextLine();
                 
-                if ( output.equals("A") ) {
+                if ( output.equals("1") ) {
                     
                     int target = 1;
                     for ( Combatant combatant : opponent_list.getMembers() ) {
-                        System.out.println(target + "is" + ": " + combatant.getName() );
-                        target++;
+                        target_id = combatant.get_name();
+                        System.out.println("the" + target + "is" + ": " + target_id);
+                        target=target+1;
                     }
-                    
-                    output = Game.getScanner().nextLine();
+                    scanner_use = Game.getScanner();
+                    output = scanner_use.nextLine();
                     find_val = Integer.parseInt(output);
                     int_zero = 0;
                     opponent_found = opponent_list.getMember(find_val);
-                    turnOrder.get(int_zero).attack(opponent_found);
+                    combatants_list.get(int_zero).attack(opponent_found);
                 }
             
             turnCounter+=turnCounter
@@ -65,44 +63,47 @@ public class Combat {
         battle();
     }
     
-    public void turnOrderP () {
+    public void combatants_list_P () {
         for (Combatant combatant : Game.getParty().living()) {
-            turnOrder.add(combatant);
+            combatants_list.add(combatant);
         }
         for (Combatant combatant : opponent_list.living()) {
-            turnOrder.add(combatant);
+            combatants_list.add(combatant);
         }
-        val_imp  = turnOrder.size();
+        val_imp  = combatants_list.size();
         for ( int i = 1 ; i < val_imp ; i++ ) {
-            Combatant hold = turnOrder.get(i);
+            Combatant hold = combatants_list.get(i);
             int divide;
             val1 = i-1;
             val2 = 0;
-            checkpt = turnOrder.get(divide);
+            checkpt = combatants_list.get(divide);
             for ( divide = val1; divide >= val2 && hold.getSpeed() < checkpt.getSpeed() ; divide--) {
-                turnOrder.set( divide + 1 , turnOrder.get(divide) );
+                combatants_list.set( divide + 1 , combatants_list.get(divide) );
             }
-            turnOrder.set(divide + 1 , hold);
+            combatants_list.set(divide + 1 , hold);
         }
     }
     
-    public void dTurnOrder () {
-        turnOrder.get(0) = new ArrayList<Combatant>();
-        turnOrder.get(1) = new ArrayList<int>();
+    public void d_combatants_list () {
+        combatants_list.get(0) = new ArrayList<Combatant>();
+        combatants_list.get(1) = new ArrayList<int>();
         
         for (Combatant new_join : party.living())
-            turnOrder[0].add(new_join);
+            combatants_list[0].add(new_join);
         for (Combatant new_join : opponent_list.living())
-            turnOrder[0].add(new_join);
-        for (Combatant new_join: turnOrder[0])
-            turnOrder[1].add(new_join.getSpeed());
-        val_to_begin = turnOrder[1].size()    
+            combatants_list[0].add(new_join);
+        for (Combatant new_join: combatants_list[0])
+            combatants_list[1].add(new_join.getSpeed());
+        val_to_begin = combatants_list[1].size()    
         for (int i = 1 ; i < val_to_begin ; i++) {
             for (int j = i ; j > 0 ; j--) {
                 count_val2 = i-1;
-                if (turnOrder[1].get(i).compareTo(turnOrder[1].get(count_val2)) < 0) {
-                    turnOrder[0].set( i , turnOrder[0].set(count_val2) , turnOrder[0].get(i) );
-                    turnOrder[1].set( i , turnOrder[1].set(count_val2) , turnOrder[1].get(i) );
+                real_val = i;
+                if (combatants_list[1].get(real_val).compareTo(combatants_list[1].get(count_val2)) < 0) {
+                    new_attacker_1 = combatants_list[0].get(real_val);
+                    new_attacker_2 = combatants_list[1].get(real_val);
+                    combatants_list[0].set(real_val , combatants_list[0].set(count_val2) , new_attacker_1);
+                    combatants_list[1].set(real_val , combatants_list[1].set(count_val2) , new_attacker_2);
                 }
             }
         }
