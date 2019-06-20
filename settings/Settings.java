fb897a2dc75d2f72eb8f5f030ac207bfb141c747
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 12:40:54 -0400

    Update Settings.java

--- settings/Settings.java
+++ settings/Settings.java
@@ -1,18 +1,18 @@
 public class settings {
-    public String details;
-    public SetPlayer[] construct_pointers = new SetPlayer[3];
+    public String player_details;
+    public set_player[] construct_pointers = new set_player[3];
     
-    public settings( String construct_story, SetPlayer player_1, SetPlayer player_2, SetPlayer player_3) {
-        details = construct_story;
+    public settings( String construct_story, set_player player_1, set_player player_2, set_player player_3) {
+        player_details = construct_story;
         construct_pointers[0] = player_1;
         construct_pointers[1] = player_2;
         construct_pointers[2] = player_3;
     }
     
     public void construct_pointers() {
-        System.out.println(details);   
-        construct_pointers[0].setCharacterPoint();
-        construct_pointers[1].setCharacterPoint();
-        construct_pointers[2].setCharacterPoint();
+        System.out.println(player_details);   
+        construct_pointers[0].set_character_point();
+        construct_pointers[1].set_character_point();
+        construct_pointers[2].set_character_point();
     }   
 }
