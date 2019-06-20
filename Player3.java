e9f2c828ffbf9b6434283a80ee9c36e96a6b4f79
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 10:07:45 -0400

    Update Player3.java

--- Player3.java
+++ Player3.java
@@ -2,16 +2,15 @@ public class Slime extends Combatant implements NonPlayable {
     
     public Slime(){
         name = "player3";
-        pacts = "none";
+        weakness = "none";
         lvl = 10;
         max_health = 125;
         max_energy = 10;
-        strength = 4;
-        speed = 10;
-        attack1_counter = 10;
-        attack2_counter = 12;
+        player_strength = 10;
+        attack1_counter = 30;
+        attack2_counter = 50;
         accuracy = 80;
-        exp = 15;
+        exp = 10;
     }
 
     public boolean isAlive(){
@@ -20,14 +19,14 @@ public class Slime extends Combatant implements NonPlayable {
 
     public void attack(Combatant target){
         int init_resistance = target.Resistance();
-        int damage = strength * attack1_counter  - init_resistance * 5;
+        int damage = player_strength * attack1_counter  - init_resistance * 5;
         target.set_health( target.get_Health() - damage );
     }
     
     public void SlimeSmash(Combatant target){
         energy = energy - 5;
         int init_resistance = target.getResistance();
-        int damage = strength * attack2_counter  - init_resistance * 5;
+        int damage = player_strength * attack2_counter  - init_resistance * 5;
         int new_health = target.get_Health() - damage;
         target.set_health(new_health);   
     }
