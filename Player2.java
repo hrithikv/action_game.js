e0b6c729207b67fac2bfbe97cc065a7e156d9149
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 09:08:12 -0400

    Update Player2.java

--- Player2.java
+++ Player2.java
@@ -7,8 +7,8 @@ public class Player2 extends Combatant implements noplayable {
         max_health = 100;
         energy = 12;
         resistance = 7;
-        wisdom = 10;
-        speed = 50;
+        intelligence = 10;
+        pace = 50;
     }
     
     public void behavior () {
@@ -16,6 +16,7 @@ public class Player2 extends Combatant implements noplayable {
         turn_checker = max_health / 2;
         if ( Game.getCombat().getTurnCounter() % 3 == 0 )
             party_id = Game.getParty();
+            req_g_id = party_id;
             player2_attack(party_id.getMember(randomizer));
         if ( health > turn_checker )
             party_id = Game.getParty();
@@ -26,21 +27,26 @@ public class Player2 extends Combatant implements noplayable {
     }
     
     public boolean is_alive(){
+        System.out.println("Determining if alive");
         return is_alive();
     }
     
     public void attack(Combatant target){
         total_energy_attack = strength * 10;
+        System.out.println("Whoosh");
         total_resistance_attack = target.getResistance();
         int damage = total_energy_attack  - total_resistance_attack * 5;
-        target.setHealth( target.getHealth() - damage );
+        int player2_attack_adjustment = target.getHealth() - damage;
+        target.setHealth(player2_attack_adjustment);
     }
     
     public void player2_attack(Combatant target){
-        energy -= 5;
+        energy = energy - 5;
+        System.out.println("Whoosh");
         increase_damage = (int)(getStrength() - 10);
         int unweighed_damage = ((int)(Math.random() * (getStrength() - increase_damage)) + (getStrength())));
         int damage = unweighed_damage * 3;
-        target.setHealth( target.getHealth() - damage );
+        int player2_health_set = target.getHealth()-damage;
+        target.setHealth(player2_health_set);
     }
 }
