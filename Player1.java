860cc2878c8e1a3a7b231b8d0ab2cb7c99348f07
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 08:45:17 -0400

    Update Player1.java

--- Player1.java
+++ Player1.java
@@ -5,24 +5,24 @@ public class Player1 extends Playable {
     setHealth(50);
     }
 
-public void Heal(Combatant co_player){
-    energy -= 20;
+  public void Heal(Combatant co_player){
+    energy = energy- 5;
     new_health = co_player.getHealth() + 10;
     co_player.setHealth (new_health);
     }     
 
     public int Attack1(){
-        energy -= 20;
-        element = "fire";
+        energy = energy - 40;
+        element = "attack1";
         int damage_calculation = (getIntelligence() - (getIntelligence()-20))
-        int damage = 3 * ((int)(Math.random() * damage_calculation +  - 20));
+        int damage = 3 * ((int)(Math.random() * damage_calculation - 20));
         return damage;
 
     }
     
      public int Attack2(){
-        energy -= 20;
-        element = "ice";
+        energy = energy - 20;
+        element = "attack2";
         int damage_calculation = (getIntelligence() - (getIntelligence()-30))
         int damage = 3 * ((int)(Math.random() * damage_calculation +(getIntelligence() - 30)));
         return damage;
