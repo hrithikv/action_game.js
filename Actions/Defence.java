a259d875213354da23d29726c1f4c4e844808b4a
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 10:17:10 -0400

    Update Defence.java

--- Actions/Defence.java
+++ Actions/Defence.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 
 public class Defence extends Action{
     
-    public Defence(Combatant user,ArrayList<Combatant> targets_found){
-        calc = super(user,targets_found);
-        name = "DEFENCE";
+    public Defence(Combatant player,ArrayList<Combatant> targets_found){
+        calc = super(player,targets_found);
+        name = "defence";
     }
     
     public void defence () {
         
-        if (user instanceof Combatant()) {
-            resistance_val = user.getResistance() + (int)(Math.random()
-            user.setResistance(resistance_val * (20 - 10) + 6));
+        if (player instanceof Combatant()) {
+            resistance_val = player.getResistance() + (int)(Math.random())
+            player.setResistance(resistance_val * 20 + 6));
         }
-        if (user instanceof Combatant()){
-        
-            user.setResistance(resistance_val * (20 - 10) + 6));
+        if (player instanceof Combatant()){
+            player.setResistance(resistance_val * 20 + 6);
         }   
     }
 }
