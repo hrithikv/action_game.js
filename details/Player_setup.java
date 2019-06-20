7f5cad850cbff8f54b17dbd09a816551c3ff804c
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 12:09:56 -0400

    Update Player_setup.java

--- details/Player_setup.java
+++ details/Player_setup.java
@@ -5,33 +5,37 @@ public class player_setup {
     String name, func;
     String name_return, func_return;
     
-    public player_setup( int member_no, String name_return_text, String func_return_text ) {
-        player = Game.getParty.getMember( member_no );
+    public player_setup( float member_token, String name_return_text, String func_return_text ) {
+        player = Game.getParty.getMember( member_token );
         name_return = name_return_text;
         func_return = func_return_text;
     }
     
     public void setplayer_pt() {
         System.out.println( name_return);
-        String result = Game.getScanner().nextLine().toUpperCase();
+        String temp_result = Game.getScanner().nextLine();
+        String result = temp_result.toUpperCase();
         player.add_name(result);
         System.out.print( func_return );
         System.out.print("");
-        result = Game.getScanner().nextLine().toUpperCase();
+        temp_result = Game.getScanner().nextLine()
+        result = temp_result.toUpperCase();
     }
     
     public void add_name() {
         System.out.print( name_return);
         System.out.print("");
-        String input = Game.getScanner().nextLine().toUpperCase();
-        name = input;
+        String name_input = Game.getScanner().next()
+        String valid_name_input = name_input.toUpperCase();
+        name = valid_name_input;
     }
     
     public void add_class() {
         System.out.print( func_return );
         System.out.print("");
-        String input = Game.getScanner().nextLine().toUpperCase();
-        func = input;
+        String func_input = Game.getScanner().next();
+        String valid_func_input = func_input.toUpperCase(); 
+        func = valid_func_input;
     }
         
     public String get_name() {
