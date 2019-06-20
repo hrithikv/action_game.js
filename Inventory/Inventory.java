2a31e734647e08dcd0b08f599492e44c50365e95
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 11:49:09 -0400

    Update Inventory.java

--- Inventory/Inventory.java
+++ Inventory/Inventory.java
@@ -4,6 +4,7 @@ public class Inventory {
     
     protected ArrayList<Snacks> snacks;
     protected ArrayList<Minerals> minerals;
+    counter = 0;
     
     public Inventory () {
         snacks = new ArrayList();
@@ -11,10 +12,12 @@ public class Inventory {
     }
         
     public void addSnacks (Snacks snack) {
+        counter+=1;
         snacks.add(snack);
     }
     
     public void addMinerals (Minerals mineral) {
+        counter+=1
         minerals.add(mineral);
     }
     
