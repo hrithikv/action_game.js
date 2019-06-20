35cda61d53b8b9ec790ae2b5efe44baa380db839
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-19 21:22:54 -0400

    Update Attack.java

--- Attack.java
+++ Attack.java
@@ -3,58 +3,61 @@ import java.util.Scanner;
 
 public class Attack extends Action {
     
-    public Attack (Combatant user,ArrayList<Combatant> targets) {
-        super (user,targets);
+    public Attack (Combatant self,ArrayList<Combatant> players) {
+        super (self,players);
         name = "attack";
     }
     
     public void attack () {
-        Scanner prompt = new Scanner(System.in);
+        user_input = System.in();
+        Scanner input = new Scanner(user_input);
         
-        if (user instanceof NonPlayable) {
-            if ( targets.size() == 1 ) {
-                target = targets.get(0);
-                damage = calculator();
+        if (self instanceof NonPlayable) {
+            if ( players.size() == 1 ) {
+                target = players.get(0);
+                injury = calculator();
             }
-            if ( targets.size() > 1 ) {
-                int find_val = (int) ( Math.random() * targets.size() );
-                target = targets.get( find_val );
-                damage = calculator();
-                int new_health = target.getHealth()-damage;
+            if ( players.size() > 1 ) {
+                int find_val = (int) ( Math.random() * players.size() );
+                target = players.get( find_val );
+                injury = calculator();
+                int new_health = target.getHealth()- injury;
                 target.setHealth(new_health);
             }
         }
-        if (user instanceof Playable) {
-            if ( targets.size() == 1 ) {
-                target = targets.get(0);
-                damage = calculator();
+        if (self instanceof Playable) {
+            if ( players.size() == 1 ) {
+                target = players.get(0);
+                injury = calculator();
                 target.setHealth((int) ( Math.random() * targets.size() ))
-            if ( targets.size() > 1 ) {
-                int find_target = prompt.nextInt();
-                target = targets.get(find_target);
-                damage = calculator();
-                target.setHealth(target.getHealth()-damage);
+            if ( players.size() > 1 ) {
+                int find_target = input.nextInt();
+                target = players.get(find_target);
+                injury = calculator();
+                target.setHealth(target.getHealth()- injury);
             }
         }
     }
     
     public int calculator () {
-        int difference  = user.getAccuracy() - target.getSpeed();
-        int new_val = (int) (Math.abs(difference));
+        int self_accuracy = self.getAccuracy();
+        int target_speed = target.getSpeed();
+        int contraction  = self.getAccuracy() - target.getSpeed();
+        int new_val = (int) (Math.abs(contraction));
         int fin_val = (int) (Math.pow(2,new_val));
         int random_output = (int) ( 100 / fin_val );
-        int result = (int) ( Math.random() * (100-1) + 1 );
+        int final_val = (int) ( Math.random() * (100-1) + 1 );
         
-        if (difference < 0 && result > random_output){
-            System.out.println("The Attack has been missed");
+        if (contraction < 0 && final_val > random_output){
+            System.out.println("The Attack has escaped the target");
             return;
         }
         
-        int base = (int)( user.getStrength() * (Math.random() + 0.25) );
-        int reduction = (int) (target.getResistance() * 0.75);
-        int fin_val = (int) (base-reduction);
+        int init = (int)( self.getStrength() * (Math.random() + 0.25) );
+        int removal = (int) (target.getResistance() * 0.75);
+        int fin_val = (int) (init-removal);
         
-        if (difference > 0 && result < random_output)
+        if (contraction > 0 && final_val < random_output)
             return 2 * (fin_val);       
         else
             return (fin_val);  
