144a0d876f45bac3ba3e46f54bc67aaed5f791e9
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-20 08:04:25 -0400

    Update Decision_pt.java

--- Decision_pt.java
+++ Decision_pt.java
@@ -2,28 +2,27 @@ import java.util.Scanner;
 
 public class Decision_pt {
     
-    public Scanner user;
-    public String decisionText;
-    public String[] options = new String[3];
+    public Scanner player;
+    public int total_players = 3;
+    public String player_choice;
+    public String[] player_options = new String[total_players];
     public Outcome answer;
     
-    public Decision_pt( String decision, String option1, String option2, String option3, Outcome outcome ) {
-        decisionText = decision;  
+    public Decision_pt( String decision, String choice1, String choice2, String choice3, Outcome outcome ) {
+        player_choice = decision;  
         int one = 1;
         int zero = 0;
         int two = 2;
-        options[zero] = option1;
-        options[one] = option2;
-        options[two] = option3;
+        player_options[zero] = choice1;
+        player_options[one] = choice2;
+        player_options[two] = choice3;
         
         response = outcome;
     }
     
     public String get_Decision() {
         int one = 1;
-        int zero = 0;
-        int two = 2;
-        return decisionText + "\n" + options[zero] + "\n" + options[one] + "\n" + options[two] + "\n";
+        return player_choice + "\n" + player_options[zero] + "\n" + player_options[one] + "\n" + player_options[two] + "\n";
     }
     
     public String get_Response( String Input_val ) {
@@ -35,7 +34,8 @@ public class Decision_pt {
     }
     
     public int  makeDecision() {
-        String output = Game.Scanner().next();
+        scanner = Game.Scanner();
+        String output = scanner.next();
         System.out.println(get_response(output));
         return change_morality(output);
     }
