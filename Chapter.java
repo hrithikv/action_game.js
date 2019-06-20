a1b4d78e2899fbee97dbcfdb07316d58850daf05
hrithikv <35966198+hrithikv@users.noreply.github.com>
2019-06-19 22:47:28 -0400

    Update Chapter.java

--- Chapter.java
+++ Chapter.java
@@ -1,19 +1,19 @@
 public class Chapter {
-    public ChapterIntro intro_pt;
-    public DecisionPoint[] decisions = new DecisionPoint[3];
-    public int moralityChange;
+    public Intro_chapter intro_cpt;
+    public Decision_pt[] selections = new Decision_pt[3];
+    public int morality_change;
     
-    public Chapter( ChapterIntro intro_chptr, DecisionPoint d1, DecisionPoint d2, DecisionPoint d3 ) {
-        intro_pt = intro_chptr;
-        decisions[0] = d1;
-        decisions[1] = d2;
-        decisions[2] = d3;
+    public Chapter( Intro_chapter intro_chptr, Decision_pt d1, Decision_pt d2, Decision_pt d3 ) {
+        intro_cpt = intro_chptr;
+        selections[0] = d1;
+        selections[1] = d2;
+        selections[2] = d3;
     }
     
     public int getChapterChange() {
-        moralityChange += decisions[0].makeDescn();
-        moralityChange += decisions[1].makeDescn();
-        moralityChange += decisions[2].makeDescn();
-        return moralityChange;
+        morality_change = morality_change + selections[0].makeDescn();
+        morality_change = morality_change + selections[1].makeDescn();
+        morality_change = morality_change + decisions[2].makeDescn();
+        return morality_change;
     }   
 }
